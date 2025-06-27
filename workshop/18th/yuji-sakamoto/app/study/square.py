7a2321c5192e7b5d09206acac80d84a1509b3efa
Yuji Sakamoto @ rpanion <yuji@sk.rdy.jp>
2025-06-27 21:17:39 +0900

    intervalReq実行位置間違い修正

--- workshop/18th/yuji-sakamoto/app/study/square.py
+++ workshop/18th/yuji-sakamoto/app/study/square.py
@@ -327,6 +327,7 @@ def setup() -> mavutil.mavfile:
 #      "127.0.0.1:14551", source_system=1, source_component=90)
 
   master.wait_heartbeat()
+  intervalReq(master)  # GLOBAL_POSITION_INTインターバル要求
   lasttime = time.monotonic()
 
   return master
@@ -561,11 +562,7 @@ def flight(master: mavutil.mavfile, delay = 1):
 if __name__ == "__main__":
     master: mavutil.mavfile = setup()
     print('接続')
-    #invalidBeep(master)
-    intervalReq(master)  # GLOBAL_POSITION_INTインターバル要求
     while True:
-        #print('before')
         flight(master)
-        #print('after')
         time.sleep(0.05)
 
